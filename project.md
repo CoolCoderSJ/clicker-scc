8ae6860d6526d920d77b52df9dfae98e530dc967
Shuchir Jain <53063247+CoolCoderSJ@users.noreply.github.com>
2025-04-01 19:19:07 +0000

    final clicker stuff

---
+++ project.md
@@ -0,0 +1,8 @@
+- number guesser 
+- connect 4 
+- tic tac toe
+- hangman
+
+- at least some buttons
+- javascript, css
+- make it fun
--- script.js
+++ script.js
@@ -11,10 +11,34 @@ document.getElementById("autoclicker")
 
 button.addEventListener("click", 
     function () {
-        score++;
-        scoreLabel.innerText = "Score: " + score;
+        score++; // score += 5
+        showScore();
 
         if (score >= 10) {
             upgradeButton.disabled = false;
         }
     })
+
+upgradeButton.addEventListener("click", function () {
+    score -= 10;
+    showScore();
+    setInterval(function () {
+        score += 20;
+        showScore();
+    }, 100);
+    upgradeButton.disabled = true;
+})
+
+function showScore () {
+    scoreLabel.innerText = "Score: " + score;
+    checkUpgrades();
+}
+
+function checkUpgrades() {
+    if (score >= 10) {
+        upgradeButton.disabled = false;
+    }
+    else {
+        upgradeButton.disabled = true;
+    }
+}
